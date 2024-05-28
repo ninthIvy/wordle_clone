bcdc28776e3f39eb5359140e6e643081794131e3
ninthIvy <136642053+ninthIvy@users.noreply.github.com>
2024-05-27 19:54:44 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,84 @@
 use std::io::{self};
 
 fn main() 
+{   let mut win_status: u8;
+    let mut buffer = String::from("");
+    let mut word_length: u8;
+    loop 
+    {
+        print!("Enter difficulty: ");
+        io::stdin().read_line(&mut buffer).expect("input failure");
+        win_status = game_loop();
+
+        if win_status == 1
+        {
+            println!("You Win!");
+        }
+        else
+        {
+            println!("You Lost!");
+        }
+
+        println!("Play again? [Y/n]");
+        io::stdin().read_line(&mut buffer).expect("input failure");
+        buffer = buffer.trim().to_string().to_uppercase();
+        if buffer != "Y"
+        {
+            println!("Bye!");
+            break;
+        }
+    }
+}
+fn string_to_u8(input: &String) -> u8
+{
+    match input.parse::<u8>()
+    {
+        Ok(u8_variable) =>
+        {
+            return u8_variable;
+        }
+        Err(_) =>
+        {
+            println!("non number passed to string_to_u8()");
+            return 255;
+        }
+    }
+}
+// 1 = success, 2 = fail
+fn game_loop() -> u8
 {
     let mut correct_letters = String::from("");
     let answer = String::from("word");
     let mut guess = String::new();
     let mut hangman = String::from("____");
-    loop 
+    let mut number_of_tries: u8 = 3;
+    let exit_condition: u8;
+    loop  
     {
         println!("Enter your guess: ");
         io::stdin().read_line(&mut guess).expect("input failure");
         guess = guess.trim().to_string();
-        check_letters(&guess, &answer, &mut correct_letters, &mut hangman);
         if guess == answer
         {
-            println!("You win");
+            exit_condition = 1;
             break;
         }
-        else 
-        {
-            println!("correct letters: {}",correct_letters);
             
+        check_letters(&guess, &answer, &mut correct_letters, &mut hangman);
+        guess.clear();
+        number_of_tries -= 1;
+        println!("correct letters: {}",correct_letters);
+        println!("hangman: {} \ntries left: {}", hangman, number_of_tries);
+
+        if number_of_tries == 0
+        {
+            exit_condition = 2;
+            break;
         }
-        println!("hangman: {}", hangman);
-        guess.clear()
     }
+
+    return exit_condition;
+
 }
 
 fn check_letters(guess: &String, answer: &String, correct_letters: &mut String, hangman: &mut String)  
